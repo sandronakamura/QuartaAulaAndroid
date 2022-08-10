d481d628d5e12822e4186fdc3a5d3bebd690aeaf
Sandro Nakamura <sandroarcadia@gmail.com>
2022-08-09 21:31:52 -0300

    Utilizando FileInputStream e FileOutputStream para persistência de dados

--- app/src/main/java/com/example/quartaaulaandroid/MainActivity.kt
+++ app/src/main/java/com/example/quartaaulaandroid/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileNotFoundException
+import java.io.IOException
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSalvar.setOnClickListener(View.OnClickListener{
-            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-            val editor = saudacaoPersistencia.edit()
+        btnSalvar.setOnClickListener(View.OnClickListener {
 
-            editor.putString("nome", txtNome.text.toString())
-            editor.putString("tratamento", listTratamento.selectedItem.toString())
-            editor.apply()
-
-            Toast.makeText(this,"Salvo com Sucesso", Toast.LENGTH_SHORT).show()
+            val data = txtNome.text.toString() + ":" + listTratamento.selectedItem.toString()
+            gravaDadoArquivo("saudacao", data)
+            Toast.makeText(this, "Salvo com Sucesso", Toast.LENGTH_SHORT).show()
         })
+
+
 
         btnExibir.setOnClickListener(View.OnClickListener {
             val intent = Intent(this, SaudacaoActivity::class.java)
             startActivity(intent)
         })
+    }
 
-
+    fun gravaDadoArquivo(filename: String, data: String) {
+        try {
+            val fs = openFileOutput(filename, Context.MODE_PRIVATE)
+            fs.write(data.toByteArray())
+            fs.close()
+        } catch (e: FileNotFoundException) {
+            Log.i("gravaDadoArquivo", "FileNotFoundException")
+        } catch (e: IOException) {
+            Log.i("gravaDadoArquivo", "IOException")
+        }
 
 
     }
--- app/src/main/java/com/example/quartaaulaandroid/SaudacaoActivity.kt
+++ app/src/main/java/com/example/quartaaulaandroid/SaudacaoActivity.kt
@@ -4,22 +4,38 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_saudacao.*
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
 
 class SaudacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudacao)
 
-        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-
-        val nome = saudacaoPersistencia.getString("nome","")
-        val tratamento = saudacaoPersistencia.getString("tratamento","")
-
+        val data = recuperaDadoArquivo("saudacao")
+        val tokenizer = StringTokenizer(data, ":")
+        val nome = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem nome"
+        val tratamento = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem tratamento"
         if(tratamento.equals("Sem tratamento")){
             lbsaudacao.text = nome
         }
         else{
             lbsaudacao.text = tratamento + " " + nome
+        }
+    }
+    fun recuperaDadoArquivo(filename: String): String{
+        try {
+            val fi = openFileInput(filename)
+            val data = fi.readBytes()
+            fi.close()
+            data.toString()
+            return data.toString(Charset.defaultCharset())
+        } catch (e: FileNotFoundException) {
+            return ""
+        } catch (e: IOException) {
+            return ""
         }
     }
 }
