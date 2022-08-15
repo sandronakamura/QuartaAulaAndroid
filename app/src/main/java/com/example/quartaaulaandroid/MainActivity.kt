7a5146342a208e0721d60f07ac7dc7f555ec1eec
Sandro Nakamura <sandroarcadia@gmail.com>
2022-08-15 10:43:55 -0300

    Adicinando comentários na MainActivity e SaudacaoActivity

--- app/src/main/java/com/example/quartaaulaandroid/MainActivity.kt
+++ app/src/main/java/com/example/quartaaulaandroid/MainActivity.kt
@@ -10,36 +10,45 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.FileNotFoundException
 import java.io.IOException
-import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Adiciona um listener que fica aguardando o clique no botão para executar o bloco de código
         btnSalvar.setOnClickListener(View.OnClickListener {
-
+            // Gera uma variável data com as informações escolhidas e envia para a funcao gravaDadoArquivo
             val data = txtNome.text.toString() + ":" + listTratamento.selectedItem.toString()
             gravaDadoArquivo("saudacao", data)
+            // Mostra uma mensagem na tela informando que foi salvo com sucesso
             Toast.makeText(this, "Salvo com Sucesso", Toast.LENGTH_SHORT).show()
         })
 
-
-
+        // Adiciona um listener que fica aguardando o clique no botão para executar o bloco de código
         btnExibir.setOnClickListener(View.OnClickListener {
+            // Mostra a intenção de abrir a SaudacaoActivity quando for acionado o botão
             val intent = Intent(this, SaudacaoActivity::class.java)
             startActivity(intent)
         })
     }
 
+    // Função para gravar dados no arquivo que recebe dois parâmetros do tipo String
     fun gravaDadoArquivo(filename: String, data: String) {
+        // Bloco try catch
         try {
+            // Cria variável que recebe o arquivo com o nome do arquivo e o contexto
             val fs = openFileOutput(filename, Context.MODE_PRIVATE)
+            // Escreve no arquivo com os dados recebidos
             fs.write(data.toByteArray())
+            // Fecha conexão com o arquivo
             fs.close()
         } catch (e: FileNotFoundException) {
+            // Caso o arquivo não seja encontrado envia um log descritivo
             Log.i("gravaDadoArquivo", "FileNotFoundException")
         } catch (e: IOException) {
+            // Caso seja um problea de entrada ou saida de dados envia um log descritivo
             Log.i("gravaDadoArquivo", "IOException")
         }
 
--- app/src/main/java/com/example/quartaaulaandroid/SaudacaoActivity.kt
+++ app/src/main/java/com/example/quartaaulaandroid/SaudacaoActivity.kt
@@ -1,6 +1,5 @@
 package com.example.quartaaulaandroid
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_saudacao.*
@@ -14,23 +13,33 @@ class SaudacaoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudacao)
 
+        // Aciona a função recuperaDadoArquivo chamando pelo nome saudacao
         val data = recuperaDadoArquivo("saudacao")
+        // Converte os dados recebidos para String
         val tokenizer = StringTokenizer(data, ":")
+        // Valida se o que foi recebido tem nome e tratamento e exibe na tela
         val nome = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem nome"
         val tratamento = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem tratamento"
         if(tratamento.equals("Sem tratamento")){
             lbsaudacao.text = nome
         }
         else{
-            lbsaudacao.text = tratamento + " " + nome
+            val saudacao = "$tratamento $nome"
+            lbsaudacao.text = saudacao
         }
     }
     fun recuperaDadoArquivo(filename: String): String{
+        // Bloco try catch
         try {
+            // Abre o arquivo com o nome recebido
             val fi = openFileInput(filename)
+            // Atribui a uma variável os dados vindos do arquivo
             val data = fi.readBytes()
+            // Fecha conexão com o arquivo
             fi.close()
+            // Converte dados para String
             data.toString()
+            // Retorna as informações como String
             return data.toString(Charset.defaultCharset())
         } catch (e: FileNotFoundException) {
             return ""
